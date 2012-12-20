a0f308de0de03333f82c94f22ece07283107fd37
Tim Haley <tphaley@gmail.com>
2012-12-20 10:30:26 -0500

    Added second @WebService ejb to module

---
+++ WebServiceEJBs/src/org/haley/tim/ejb/GoodbyeWorld.java
@@ -0,0 +1,26 @@
+package org.haley.tim.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+/**
+ * Session Bean implementation class GoodbyeWorld
+ */
+@Stateless
+@LocalBean
+@WebService
+public class GoodbyeWorld {
+
+    /**
+     * Default constructor. 
+     */
+    public GoodbyeWorld() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    String sayGoodbye() {
+    	return "Goodbye";
+    }
+
+}
